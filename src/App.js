dcceeac00a0148b0eb609fe6efef460e77f7e487
NeverLost-Never <haileyesus.mieron@gmail.com>
2021-12-09 05:30:47 +0400

    Components - Part 2

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
-import logo from "./logo.svg";
 import { Navbar, NavbarBrand } from "reactstrap";
 import Menu from "./components/MenuComponent";
 import "./App.css";
+import { DISHES } from "./shared/dishes";
+import { Component } from "react/cjs/react.production.min";
 
-function App() {
-	return (
-		<div>
-			<Navbar dark color="primary">
-				<div className="container">
-					<NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-				</div>
-			</Navbar>
+class App extends Component {
+	constructor(props) {
+		super(props);
 
-			<Menu />
-		</div>
-	);
+		this.state = {
+			dishes: DISHES,
+		};
+	}
+
+	render() {
+		return (
+			<div>
+				<Navbar dark color="primary">
+					<div className="container">
+						<NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+					</div>
+				</Navbar>
+
+				<Menu dishes={this.state.dishes} />
+			</div>
+		);
+	}
 }
 
 export default App;
