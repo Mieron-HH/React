9cbe687cb56bfb660ecab6c58ec8f1f8cddbe98a
NeverLost-Never <haileyesus.mieron@gmail.com>
2021-12-13 02:52:47 +0400

    Functional Components

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import {
 	Card,
 	CardImg,
@@ -8,67 +8,67 @@ import {
 	Media,
 } from "reactstrap";
 
-class DishDetail extends Component {
-	renderDish(SelectedDish) {
-		return (
-			<Card>
-				<CardImg top src={SelectedDish.image} alt={SelectedDish.name} />
-				<CardBody>
-					<CardTitle>{SelectedDish.name}</CardTitle>
-					<CardText>{SelectedDish.description}</CardText>
-				</CardBody>
-			</Card>
-		);
-	}
-	renderComments(Comments) {
-		if (Comments != null) {
-			const comment_details = Comments.map((comment) => {
-				return (
-					<Media key={comment.id} tag="li">
-						<p>{comment.comment}</p>
-						<p>
-							-- {comment.author} ,{" "}
-							{new Intl.DateTimeFormat("en-US", {
-								year: "numeric",
-								month: "short",
-								day: "2-digit",
-							}).format(new Date(Date.parse(comment.date)))}
-						</p>
-					</Media>
-				);
-			});
+function RenderDish({ dish }) {
+	return (
+		<Card>
+			<CardImg top src={dish.image} alt={dish.name} />
+			<CardBody>
+				<CardTitle>{dish.name}</CardTitle>
+				<CardText>{dish.description}</CardText>
+			</CardBody>
+		</Card>
+	);
+}
 
+function RenderComments({ Comments }) {
+	if (Comments != null) {
+		const comment_details = Comments.map((comment) => {
 			return (
-				<div>
-					<h4>Comments</h4>
-					{comment_details}
-				</div>
+				<Media key={comment.id} tag="li">
+					<p>{comment.comment}</p>
+					<p>
+						-- {comment.author} ,{" "}
+						{new Intl.DateTimeFormat("en-US", {
+							year: "numeric",
+							month: "short",
+							day: "2-digit",
+						}).format(new Date(Date.parse(comment.date)))}
+					</p>
+				</Media>
 			);
-		} else {
-			return <div></div>;
-		}
-	}
-	render() {
-		const SelectedDish = this.props.dish;
-		if (SelectedDish != null) {
-			return (
-				<div className="container mb-2">
-					<div className="row">
-						<div className="col-12 col-md-5 m-1">
-							{this.renderDish(SelectedDish)}
-						</div>
-						<div className="col-12 col-md-5 m-1">
-							<Media list className="list-unstyled">
-								{this.renderComments(SelectedDish.comments)}
-							</Media>
-						</div>
-					</div>
-				</div>
-			);
-		} else {
-			return <div></div>;
-		}
+		});
+
+		return (
+			<div>
+				<h4>Comments</h4>
+				{comment_details}
+			</div>
+		);
+	} else {
+		return <div></div>;
 	}
 }
+
+const DishDetail = (props) => {
+	const SelectedDish = props.dish;
+	if (SelectedDish != null) {
+		return (
+			<div className="container mb-2">
+				<div className="row">
+					<div className="col-12 col-md-5 m-1">
+						<RenderDish dish={SelectedDish} />
+					</div>
+					<div className="col-12 col-md-5 m-1">
+						<Media list className="list-unstyled">
+							<RenderComments Comments={SelectedDish.comments} />
+						</Media>
+					</div>
+				</div>
+			</div>
+		);
+	} else {
+		return <div></div>;
+	}
+};
 
 export default DishDetail;
