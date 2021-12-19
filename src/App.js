372e65a7d7cff52f3788dd9c566eda0d40a992dc
NeverLost-Never <haileyesus.mieron@gmail.com>
2021-12-19 04:53:47 +0400

    React Router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
+import React, { Component } from "react";
 import Main from "./components/MainComponent";
 import "./App.css";
-import { Component } from "react/cjs/react.production.min";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 class App extends Component {
 	render() {
 		return (
-			<div>
-				<Main />
-			</div>
+			<BrowserRouter>
+				<div>
+					<Main />
+				</div>
+			</BrowserRouter>
 		);
 	}
 }
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,10 +1,11 @@
-import { Component } from "react/cjs/react.production.min";
-
+import React, { Component } from "react";
+import Home from "./HomeComponent";
 import Menu from "./MenuComponent";
 import DishDetail from "./DishdetailComponent";
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
 import { DISHES } from "../shared/dishes";
+import { Switch, Route, Navigate, Redirect } from "react-router-dom";
 
 class Main extends Component {
 	constructor(props) {
@@ -12,34 +13,26 @@ class Main extends Component {
 
 		this.state = {
 			dishes: DISHES,
-			selectedDish: null,
 		};
 	}
 
-	onDishSelect(dishID) {
-		this.setState({ selectedDish: dishID });
-		console.log(
-			"check check",
-			this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)
-		);
-	}
-
 	render() {
+		const HomePage = () => {
+			return <Home />;
+		};
+
 		return (
 			<div>
 				<Header />
-				<Menu
-					dishes={this.state.dishes}
-					onClick={(dishId) => this.onDishSelect(dishId)}
-				/>
-
-				<DishDetail
-					dish={
-						this.state.dishes.filter(
-							(dish) => dish.id === this.state.selectedDish
-						)[0]
-					}
-				/>
+				<Switch>
+					<Route path="/home" component={HomePage} />
+					<Route
+						exact
+						path="/menu"
+						component={() => <Menu dishes={this.state.dishes} />}
+					/>
+					<Redirect path="/" to="/home" />
+				</Switch>
 				<Footer />
 			</div>
 		);
